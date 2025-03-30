db25e62374922ab810fccb4b0df607f3a4d60f78
Andrew Imredy <andrew@freedomgateway.llc>
2025-03-30 03:42:07 -0400

    let img

--- popup.ts
+++ popup.ts
@@ -11,8 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     //NB: chrome.tabs.update(tabId, {muted});
 
     let capturePromise = chrome.tabs.captureVisibleTab();
-    capturePromise.then(dataUrl => {
-      alert(dataUrl);
+    capturePromise.then((b64: string) => {
+      let img = new Image();
+      img.src = b64;
+
+      alert(b64);
     })
       .catch(error => {
         console.error("Error capturing screenshot:", error);
