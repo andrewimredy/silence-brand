e33312846118aae55a2fe4060f90e5fbf70da665
Andrew Imredy <andrew@freedomgateway.llc>
2025-03-30 16:26:07 -0400

    display img and action bttns

--- popup.ts
+++ popup.ts
@@ -1,21 +1,34 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   let screenshotButton = document.getElementById('screenshotButton');
+  let frame = document.getElementById('ssPlaceholder');
+  let saveDiv = document.getElementById('saveBtnContainer');
+  let saveAdBtn = document.getElementById('saveAd');
+  let saveBroadcastBtn = document.getElementById('saveBroadcast');
   
   if (!(screenshotButton instanceof HTMLElement)) {
     alert('screenshotButton not found or invalid type');
     return;
   }
+  if (!(frame instanceof Image)){
+    alert("image frame not found");
+    return;
+  }
+  if (!(saveDiv instanceof HTMLElement)){
+    alert("save button container not found!")
+    return;
+  }
+
+
 
   screenshotButton.addEventListener("click", (event) => {
     //NB: chrome.tabs.update(tabId, {muted});
 
     let capturePromise = chrome.tabs.captureVisibleTab();
     capturePromise.then((b64) => {
-      let img = new Image();
-      img.src = b64;
-      alert('got image');
-    })
+        frame.src = b64;
+        saveDiv.style.display = 'flex';
+      })
       .catch(error => {
         console.error("Error capturing screenshot:", error);
         alert("Error capturing screenshot: " + error.message);
